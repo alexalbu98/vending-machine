12707b23abe9ca9ec09b64eb38139d5243dca115
Alex Albu <alexalbu78@gmail.com>
2025-06-01 21:49:35 +0300

    Initial commit

---
+++ src/test/java/me/alex/vendingmachine/JunitTest.java
@@ -0,0 +1,13 @@
+package me.alex.vendingmachine;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class JunitTest {
+
+  @Test
+  void firstTest() {
+    assertTrue(true);
+  }
+}
