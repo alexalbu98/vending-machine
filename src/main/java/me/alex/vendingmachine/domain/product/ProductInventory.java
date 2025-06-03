9fd6f4eda553814a0b87b59d4d171d2c7270e993
Alex Albu <alexalbu78@gmail.com>
2025-06-03 08:39:59 +0300

    Updated Product inventory constrains

--- src/main/java/me/alex/vendingmachine/domain/product/ProductInventory.java
+++ src/main/java/me/alex/vendingmachine/domain/product/ProductInventory.java
@@ -1,11 +1,7 @@
 package me.alex.vendingmachine.domain.product;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-@AllArgsConstructor
 @Getter
 public class ProductInventory {
 
@@ -13,6 +9,35 @@ public class ProductInventory {
   private final int maxQuantity;
   private int quantity;
   private final Integer code;
+
+  public ProductInventory(Product product, int maxQuantity, Integer code) {
+    verifyArguments(quantity, maxQuantity, code);
+    this.product = product;
+    this.maxQuantity = maxQuantity;
+    this.code = code;
+  }
+
+  public ProductInventory(Product product, int maxQuantity, int quantity, Integer code) {
+    verifyArguments(quantity, maxQuantity, code);
+    this.product = product;
+    this.maxQuantity = maxQuantity;
+    this.code = code;
+    this.quantity = quantity;
+  }
+
+  private void verifyArguments(int quantity, int maxQuantity, int code) {
+    if (maxQuantity <= 0) {
+      throw new IllegalArgumentException("Maximum quantity must be greater than zero");
+    }
+    if (quantity < 0 || quantity > maxQuantity) {
+      throw new IllegalArgumentException(
+          "Quantity must be greater or equal to zero and less than or equal to maximum quantity: "
+              + maxQuantity);
+    }
+    if (code < 10) {
+      throw new IllegalArgumentException("Product code must be greater than or equal to 10");
+    }
+  }
 
   public void increaseQuantity(int amount) {
     if (amount < 0) {
@@ -26,6 +51,10 @@ public class ProductInventory {
   }
 
   public void increaseQuantity() {
+    if (this.quantity + 1 > maxQuantity) {
+      throw new IllegalArgumentException(
+          "Cannot increase product quantity beyond maximum available space: " + maxQuantity);
+    }
     this.quantity++;
   }
 
