ee20d5c8250db6abe4ee1986e02cfc66baf56715
Alex Albu <alexalbu78@gmail.com>
2025-06-03 23:10:53 +0300

    Implemented refund state

--- src/main/java/me/alex/vendingmachine/domain/state/RefundingState.java
+++ src/main/java/me/alex/vendingmachine/domain/state/RefundingState.java
@@ -1,13 +1,15 @@
 package me.alex.vendingmachine.domain.state;
 
+import java.math.BigDecimal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.alex.vendingmachine.domain.VendingMachine;
+import me.alex.vendingmachine.domain.change.Change;
 
 @RequiredArgsConstructor
 public class RefundingState implements VendingMachineState {
 
-  private final VendingMachine VendingMachine;
+  private final VendingMachine vendingMachine;
 
   @Override
   public String getStateMessage() {
@@ -21,7 +23,30 @@ public class RefundingState implements VendingMachineState {
 
   @Override
   public String stateAction() {
-    return "";
+    var change = vendingMachine.refund();
+    if (vendingMachine.getCurrentCredit().compareTo(BigDecimal.ZERO) == 0) {
+      vendingMachine.setState(new IdleState(vendingMachine));
+    } else {
+      vendingMachine.setState(new CoinInsertedState(vendingMachine));
+    }
+    return formatRefundMessage(change);
+  }
+
+  private String formatRefundMessage(List<Change> change) {
+    StringBuilder stringBuilder = new StringBuilder();
+    change.forEach(c ->
+        stringBuilder.append("Refunded ")
+            .append(c.quantity())
+            .append(" coins of value ")
+            .append(c.coin().value()).append("\n")
+    );
+    if (vendingMachine.getCurrentCredit().compareTo(BigDecimal.ZERO) == 0) {
+      stringBuilder.append("Credit refunded successfully.");
+    } else {
+      stringBuilder.append("Could not refund all credit, not enough change! Remaining credit: ")
+          .append(vendingMachine.getCurrentCredit());
+    }
+    return stringBuilder.toString();
   }
 
   @Override
